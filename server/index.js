9f5a4ffb6cc7200c70b9ace88e0ec277972bd83e
Ratik Vig <ratikvig@gmail.com>
2023-02-05 01:19:59 -0500

    change default port

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 300
 
 app.listen(PORT, () => console.log(`Server listening at port ${PORT}`))
