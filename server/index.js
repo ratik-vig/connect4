945d98c463903f7daa26f30baba135f14faa55eb
Ratik vig <ratikvig@ratiks-mbp.mynetworksettings.com>
2023-02-05 01:09:28 -0500

    added env file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => console.log(`Server listening at port 3000`))
+app.listen(PORT, () => console.log(`Server listening at port ${PORT}`))
