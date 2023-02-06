cf65030f16b266234ef84274591d65470710812d
Ratik Vig <ratikvig@gmail.com>
2023-02-06 12:26:55 -0500

    socket server setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
 const dotenv = require('dotenv')
 
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 300
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.listen(PORT, () => console.log(`Server listening at port ${PORT}`))
+const PORT = process.env.PORT || 3000
+
+io.on('connection', (socket) => {
+    console.log('client connected')
+    socket.on('disconnect', () => {
+        console.log('client disconnected')
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send('Home route')
+})
+
+server.listen(PORT, () => console.log(`Server listening at port ${PORT}`))
